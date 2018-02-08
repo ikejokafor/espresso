55458e2f53c39ef17aaebec72bf74efdf70c30dc
IkennaOkafor <ikennajeshuaokafor@gmail.com>
2018-02-07 20:57:07 -0500

    Updates

--- example/src/espressoTesterMain.cpp
+++ example/src/espressoTesterMain.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **agrv) {
 	string protoTxt = "/home/ikenna/caffe-master/models/dcNet/deploy_sqz.prototxt";
 	vector<layerInfo_t> layerInfo = parseCaffeData(protoTxt, " ");
 	Network *network = new Network(layerInfo);
-	Blob_t inputBlob.data = (float*)malloc(network->m_cnn[0].m_inputDepth * network->m_cnn[0]->m_numInputRows * network->m_cnn[0]->m_numInputCols * sizeof(float));
+	Blob_t inputBlob.data = (float*)malloc(network->m_cnn[0]->m_inputDepth * network->m_cnn[0]->m_numInputRows * network->m_cnn[0]->m_numInputCols * sizeof(float));
 	Blob_t outputBlob = (float*)malloc(network->m_cnn[m_cnn.size() - 1]->m_outputDepth * network->m_cnn[0]->m_numOutputRows * network->m_cnn[0]->m_numOutputCols * sizeof(float));
 	network->Forward(inputBlob, &outputBlob);
 
