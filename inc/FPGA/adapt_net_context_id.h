07ecdec6f8b5297cb133f7d82cbacfd3bf006c9f
Ikenna Okafor <ikennajeshuaokafor@gmail.com>
2020-02-14 23:23:35 -0500

    Updates

--- inc/FPGA/adapt_net_context_id.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-typedef struct _adapt_net_context_id {
-	int unit_id;
-	int context_id;
-	_adapt_net_context_id() {};
-	_adapt_net_context_id(int uid, int cid) : unit_id(uid), context_id(cid) {}
-}adapt_net_context_id;
