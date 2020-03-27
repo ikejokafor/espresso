458fc3fbaa6f0ad929988dfa99c3ed3cd04b93fb
Ikenna Okafor <ikennajeshuaokafor@gmail.com>
2020-03-27 18:04:55 -0400

    Updates

--- inc/FPGA/common.hpp
+++
@@ -1,9 +0,0 @@
-#define SYSTEMC
-#define NUM_FAS				1
-#define MAX_AWP_PER_FAS     1
-#define MAX_QUAD_PER_AWP    4
-#define NUM_QUAD_PER_AWP	1
-#define NUM_TOTAL_QUADS     (MAX_AWP_PER_FAS * NUM_QUAD_PER_AWP)
-#define QUAD_MAX_KERNELS	64
-#define QUAD_MAX_DEPTH		8
-#define QUAD_DPTH_SIMD		(QUAD_MAX_DEPTH * NUM_TOTAL_QUADS)
